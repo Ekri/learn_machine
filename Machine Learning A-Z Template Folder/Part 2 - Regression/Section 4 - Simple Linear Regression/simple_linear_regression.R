25cf0418965492cfbff82827fc9e0de8d0849ecb
Bartosz Maciega <b.maciega@futuremind.com>
2017-09-27 08:55:11 +0200

    multiple linear regression

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.R
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.R
@@ -15,31 +15,27 @@ test_set = subset(dataset, split == FALSE)
 # training_set = scale(training_set)
 # test_set = scale(test_set)
 
-# Fitting Simple Linear Regression to the Training set
-regressor = lm(formula = Salary ~ YearsExperience,
-               data = training_set)
+#Fitting
+regressor = lm(formula = Salary ~ YearsExperience, data = training_set)
 
-# Predicting the Test set results
-y_pred = predict(regressor, newdata = test_set)
+#Prediction
+y_pred = predict(regressor,newdata = test_set)
 
-# Visualising the Training set results
+#Visualising
 library(ggplot2)
-ggplot() +
-  geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary),
-             colour = 'red') +
-  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
-            colour = 'blue') +
-  ggtitle('Salary vs Experience (Training set)') +
-  xlab('Years of experience') +
-  ylab('Salary')
+ggplot() + geom_point(aes(x=training_set$YearsExperience, y=training_set$Salary),
+                      colour = 'red') +
+          geom_line(aes(x=training_set$YearsExperience, y = predict(regressor,newdata = training_set)),
+                    colour ='blue') +
+  ggtitle("Salary vs Experience (Training set)") +
+  xlab("Years of experience") +
+  ylab("Salary")
 
-# Visualising the Test set results
 library(ggplot2)
-ggplot() +
-  geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary),
-             colour = 'red') +
-  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
-            colour = 'blue') +
-  ggtitle('Salary vs Experience (Test set)') +
-  xlab('Years of experience') +
-  ylab('Salary')
+ggplot() + geom_point(aes(x=test_set$YearsExperience, y=test_set$Salary),
+                      colour = 'red') +
+  geom_line(aes(x=training_set$YearsExperience, y = predict(regressor,newdata = training_set)),
+            colour ='blue') +
+  ggtitle("Salary vs Experience (Test set)") +
+  xlab("Years of experience") +
+  ylab("Salary")
--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
@@ -1,7 +1,6 @@
 # Simple Linear Regression
 
 # Importing the libraries
-import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -13,7 +12,7 @@ y = dataset.iloc[:, 1].values
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1 / 3, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
@@ -25,4 +24,26 @@ y_train = sc_y.fit_transform(y_train)"""
 
 # Fitting Simple Linear Regression to the Training set
 from sklearn.linear_model import LinearRegression
+
 regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+print "test: ", y_test
+print "predict: ", y_pred
+
+# Visualising
+# plt.scatter(X_train, y_train, color='red')
+# plt.plot(X_train, regressor.predict(X_train), color='blue')
+# plt.title('Salary vs Experience(Training Set)')
+# plt.xlabel('Years of experience')
+# plt.ylabel('Salary')
+# plt.show()
+
+plt.scatter(X_test, y_test, color='red')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.title('Salary vs Experience(Test Set)')
+plt.xlabel('Years of experience')
+plt.ylabel('Salary')
+plt.show()
