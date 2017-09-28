7fa563b918ccc8ae56774fdb8d1ddcd9e5664017
Bartosz Maciega <b.maciega@futuremind.com>
2017-09-28 10:21:22 +0200

    python backward elimination

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -45,3 +45,12 @@ print regressor.score(X_test, y_test)
 
 # # Predicting the Test set results
 # y_pred = regressor.predict(X_test)
+
+
+# Building the optimal model using Backward Elimination
+import statsmodels.formula.api as sm
+
+X = np.append(arr=np.ones((50, 1)).astype(int), values=X, axis=1)
+X_opt = X[:, [0, 3, 5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+print regressor_OLS.summary()
