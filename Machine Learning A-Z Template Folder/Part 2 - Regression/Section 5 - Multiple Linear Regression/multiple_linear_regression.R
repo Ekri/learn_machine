1622811da2b72ff89b00a80b23fe411d58aeca1c
Bartosz Maciega <b.maciega@futuremind.com>
2017-09-28 12:07:23 +0200

    regression in R

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.R
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.R
@@ -8,14 +8,17 @@ dataset$State = factor(dataset$State,
                        levels = c('New York', 'California', 'Florida'),
                        labels = c(1, 2, 3))
 
+
 # Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
-# library(caTools)
-# set.seed(123)
-# split = sample.split(dataset$Profit, SplitRatio = 0.8)
-# training_set = subset(dataset, split == TRUE)
-# test_set = subset(dataset, split == FALSE)
-# 
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Profit, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+regressor = lm(formula = Profit ~ ., data = training_set )
+
+y_pred = predict(regressor,newdata = test_set)
+
 # # Feature Scaling
 # # training_set = scale(training_set)
 # # test_set = scale(test_set)
