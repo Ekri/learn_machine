92dc38904aa373702884598c9eca3ce66a84c8df
Bartosz Maciega <b.maciega@futuremind.com>
2017-09-29 10:43:46 +0200

    polynomial regression visualising

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.py
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.py
@@ -35,7 +35,7 @@ plt.plot(X, linear_regressor.predict(X), color='blue')
 plt.title('Truth or Bluff (Linear Regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
-plt.show()
+# plt.show()
 
 # # Visualising the Polynomial Regression results
 X_grid = np.arange(min(X), max(X), 0.1)
@@ -46,23 +46,11 @@ plt.plot(X_grid, linear_regressor_2.predict(polynomial_regression.fit_transform(
 plt.title('Truth or Bluff (Polynomial Regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
-plt.show()
-
-# # Visualising the Polynomial Regression results (for higher resolution and smoother curve)
-
-
-
-# X_grid = np.arange(min(X), max(X), 0.1)
-# X_grid = X_grid.reshape((len(X_grid), 1))
-# plt.scatter(X, y, color = 'red')
-# plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
-# plt.title('Truth or Bluff (Polynomial Regression)')
-# plt.xlabel('Position level')
-# plt.ylabel('Salary')
 # plt.show()
-#
+
 # # Predicting a new result with Linear Regression
-# lin_reg.predict(6.5)
-#
+print linear_regressor.predict(6.5)
+
 # # Predicting a new result with Polynomial Regression
-# lin_reg_2.predict(poly_reg.fit_transform(6.5))
+print linear_regressor_2.predict(polynomial_regression.fit_transform(6.5))
+
