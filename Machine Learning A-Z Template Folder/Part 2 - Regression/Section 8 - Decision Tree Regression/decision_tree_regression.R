3fd70e934ac3c4ded4e540d26f4fa57d222670d7
Bartosz Maciega <b.maciega@futuremind.com>
2017-10-03 08:54:27 +0200

    random forest r

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 8 - Decision Tree Regression/decision_tree_regression.R
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 8 - Decision Tree Regression/decision_tree_regression.R
@@ -4,22 +4,8 @@
 dataset = read.csv('Position_Salaries.csv')
 dataset = dataset[2:3]
 
-# Splitting the dataset into the Training set and Test set
-# # install.packages('caTools')
-# library(caTools)
-# set.seed(123)
-# split = sample.split(dataset$Salary, SplitRatio = 2/3)
-# training_set = subset(dataset, split == TRUE)
-# test_set = subset(dataset, split == FALSE)
-
-# Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
-
-# Fitting Decision Tree Regression to the dataset
-# install.packages('rpart')
 library(rpart)
-regressor = rpart(formula = Salary ~ .,
+regressor = rpart(formula = Salary ~.,
                   data = dataset,
                   control = rpart.control(minsplit = 1))
 
@@ -39,6 +25,29 @@ ggplot() +
   xlab('Level') +
   ylab('Salary')
 
-# Plotting the tree
-plot(regressor)
-text(regressor)
+
+# Splitting the dataset into the Training set and Test set
+# # install.packages('caTools')
+# library(caTools)
+# set.seed(123)
+# split = sample.split(dataset$Salary, SplitRatio = 2/3)
+# training_set = subset(dataset, split == TRUE)
+# test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
+
+# Fitting Decision Tree Regression to the dataset
+# install.packages('rpart')
+# library(rpart)
+# regressor = rpart(formula = Salary ~ .,
+#                   data = dataset,
+#                   control = rpart.control(minsplit = 1))
+# 
+
+# 
+
+# # Plotting the tree
+# plot(regressor)
+# text(regressor)
